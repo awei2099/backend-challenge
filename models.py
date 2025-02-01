2eec8b6499ce335df096063452396e791e999a6d
Andy Wei <67355062+awei2099@users.noreply.github.com>
2025-02-01 11:53:57 -0500

    Update models.py

--- models.py
+++ models.py
@@ -3,3 +3,24 @@ from app import db
 # Your database models should go here.
 # Check out the Flask-SQLAlchemy quickstart for some good docs!
 # https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+                              
+
+class Club(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    tags = db.Column(db.JSON)
+    favorite_count = db.Column(db.Integer, default=0)  
+
+    def __repr__(self):
+        return f'<Club {self.name}>'
